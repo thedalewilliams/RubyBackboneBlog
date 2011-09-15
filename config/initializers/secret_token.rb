dd274e24b67177630b5b96f0467c4e483bf71232
Dale Williams <enslam@ubuntu.(none)>
2011-09-15 02:12:09 -0700

    Created RubyBackboneBlog

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyBackboneBlog::Application.config.secret_token = '47baeaa64816b1f06aa63c55886dd6ba01a29c48a7c82a9ea0b29954ec2b8631530e05cd3330f60822504fb9a7e36c91f424a7cb605b8ba6d09d41485ef5170f'
